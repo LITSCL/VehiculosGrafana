c5aa625e111e9b5e6eb300118ea72a0a0ac3492e
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-10 18:53:55 -0300

    Ok

--- SQL-Clear/dbvehiculosgrafana.sql
+++ SQL-Clear/dbvehiculosgrafana.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-09-2023 a las 22:52:46
+-- Tiempo de generación: 10-12-2023 a las 22:47:20
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.0
 
--- SQL-Full/dbvehiculosgrafana.sql
+++ SQL-Full/dbvehiculosgrafana.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-09-2023 a las 16:38:50
+-- Tiempo de generación: 10-12-2023 a las 22:48:44
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.0
 
